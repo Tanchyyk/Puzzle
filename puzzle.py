b000b6382cfb887425b78b806cf54d0b622b6e96
Tania Prysiazhnyk <73783964+Tanchyyk@users.noreply.github.com>
2021-02-08 20:57:27 +0200

    Second version

--- puzzle.py
+++ puzzle.py
@@ -1,39 +1,58 @@
+"""
+https://github.com/Tanchyyk/Puzzle.git
+"""
+import doctest
+
+
 def validate_board(board: list) -> bool:
+    """
+    Function validate a board of a game.
+    Return True if board is ready for the game, False - in other case.
+    >>> validate_board(["**** ****", "***1 ****", "**  3****", "* 4 1****",\
+    "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"])
+    False
+    """
     if validate_row(board) and validate_column(board) and validate_color(board):
         return True
     return False
 
 
 def validate_row(board: list) -> bool:
+    """
+    Function check if rows of a board are built correctly.
+    Return True if it is, False - if it is not.
+    """
     for row in board:
-        list_of_nums = []
-        for el in row:
-            if el.isnumeric():
-                list_of_nums.append(int(el))
-        if len(set(list_of_nums)) == len(list_of_nums):
+        for element in row:
+            if row.count(element) > 1:
+                return False
             return True
 
 
 def validate_column(board: list) -> bool:
+    """
+    Function check if columns of a board are built correctly.
+    Return True if it is, False - if it is not.
+    """
     for index in range(len(board[0])):
-        list_of_nums = []
         for row in board:
-            if row[index].isnumeric():
-                list_of_nums.append(int(row[index]))
-        if len(set(list_of_nums)) != len(list_of_nums):
-            return False
+            if row.count(row[index]) > 1 and row[index].isnumeric():
+                return False
     return True
 
 
 def validate_color(board: list) -> bool:
+    """
+    Function check if all blocks in one color have not the same numbers inside.
+    """
     for i in range(1, len(board[0]) + 1):
         list_of_nums = []
         for row in board:
             if row[i - 1].isnumeric():
                 list_of_nums.append(int(row[i - 1]))
-        for el in board[-i]:
-            if el.isnumeric():
-                list_of_nums.append(int(el))
+        for element in board[-i]:
+            if element.isnumeric():
+                list_of_nums.append(int(element))
         if len(set(list_of_nums)) != len(list_of_nums):
             return False
     return True
@@ -46,9 +65,9 @@ board = [
  "* 4 1****",
  "     9 5 ",
  " 6  83  *",
- "3   6  **",
+ "3   1  **",
  "  8  2***",
  "  2  ****"
 ]
-
-print(validate_board(board))
+# print(validate_board(board))
+print(doctest.testmod())
